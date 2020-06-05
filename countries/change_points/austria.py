01c1c89e6e9415986725f662d82de9065ce81839
Sebastian <sebastian@mohrenclan.de>
2020-06-05 15:07:16 +0200

    WIP change points for austria, already some file/data structure

---
+++ countries/change_points/austria.py
@@ -0,0 +1,25 @@
+""" # Austria
+    
+The change points are mostly copied from
+https://github.com/Konrad982/covid19_inference/blob/master/scripts/Austria_JHU.ipynb.
+
+These were chosen by using the [Wikipedia timeline](https://en.wikipedia.org/wiki/2020_coronavirus_pandemic_in_Austria)
+and the [Imperial College Report](https://www.imperial.ac.uk/media/imperial-college/medicine/mrc-gida/2020-03-30-COVID19-Report-13.pdf).
+
+"""
+
+
+"""
+We choose the following change points:
+* __10/03__ Ban on public events
+    All outdoor events with more than 500 people and all indoor events with more than 100 people were cancelled.
+    
+* __16/03__ Schools closed
+    We assume school closures to take effect on Monday together with the strong social distancing
+    [ref](https://www.kleinezeitung.at/politik/5783037/Coronavirus_Stufenweise-ab-Montag_Oesterreich-schliesst-Schulen)
+
+* __30/03__ Mask obligations in stores
+
+* __30/04__ Rules relaxed but also further mask obligation
+
+"""
---
+++ countries/change_points/change_points.py
@@ -0,0 +1,20 @@
+# ------------------------------------------------------------------------------ #
+# @Author:        Sebastian B. Mohr
+# @Email:         
+# @Created:       2020-06-05 11:25:02
+# @Last Modified: 2020-06-05 11:28:46
+# ------------------------------------------------------------------------------ #
+
+
+def get_cps(countries):
+    """
+    Function that returns all change points for different countries.
+    They are loaded from there corresponding country python files.
+
+    Parameters
+    ----------
+    countries : array, string
+
+    """ 
+
+
