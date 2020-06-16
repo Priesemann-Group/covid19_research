f12c5df1b7e4d798ebba59f8a4800437b7c5a9ae
Konrad982 <konrad-dierks@gmx.de>
2020-06-16 23:51:56 +0200

    Ban on gatherings of >1000 people added, comment on school closures, should there be a change point for the closure of non-essential services (one day earlier?)

--- countries/measures/data/france.py
+++ countries/measures/data/france.py
@@ -22,16 +22,24 @@ Personal following of press
 """
 measures = []  # Create empty array on which we append later on
 
+
 """
-March 13:
+March 10:
 """
-begin = datetime.datetime(2020, 3, 13)
+begin = datetime.datetime(2020, 3, 10)  # according to Wikipedia
 keyword = dict(
-    tag="forbid assemblies > 100",
-    description=""" Bans of events >100 people."""
-    )
+    tag="forbid assemblies > 1000", description=""" Bans of events >1000 people."""
+)
 measures.append(Measure(keyword, begin))
 
+"""
+March 14:
+"""
+begin = datetime.datetime(2020, 3, 14)  # according to Wikipedia
+keyword = dict(
+    tag="forbid assemblies > 100", description=""" Bans of events >100 people."""
+)
+measures.append(Measure(keyword, begin))
 
 """
 March 16:
@@ -39,8 +47,8 @@ March 16:
 begin = datetime.datetime(2020, 3, 16)
 keyword = dict(
     tag=kw.close_schools,
-    description=""" Nationwide school closures became effectiv.""" # announced on saturday, 14/03
-    )
+    description=""" Nationwide school closures became effective, including universities.""",  # announced on saturday, 14/03, falls essentially together with closure of non-essential services, effective one day earlier
+)
 measures.append(Measure(keyword, begin))
 
 
@@ -51,6 +59,6 @@ begin = datetime.datetime(2020, 3, 17)
 keyword = dict(
     tag=kw.lockdown,
     description=""" Everybody has to stay at home. Need a selfauthorisation
-                    form to leave home. """
-    )
+                    form to leave home. """,
+)
 measures.append(Measure(keyword, begin))
