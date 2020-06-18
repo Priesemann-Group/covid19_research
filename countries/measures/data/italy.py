aa6013e87b97a840067a24c3050b742a4f7ae4c9
Konrad982 <konrad-dierks@gmx.de>
2020-06-18 23:26:03 +0200

    Some corrections and many comments Italy - still work to do, especially in taking account earlier regional measures in the popolous and largely affected nothern part of Italy

--- countries/measures/data/italy.py
+++ countries/measures/data/italy.py
@@ -26,24 +26,68 @@ January 30:
 begin = datetime.datetime(2020, 1, 30)
 keyword = dict(
     tag="cancel flights China",
-    description=""" The President then declared that, on the orders of the national
-                    health authorities,  all flights to and from China  - in addition
-                    to those from Wuhan, already suspended by the Chinese authorities
-                    - were suspended and reassured that the situation is under control
-                    and that the measures taken are precautionary and place Italy
-                    at the highest level of caution on an international level."""
-    )
+    description=""" The Prime Minister declared that, on the orders of the national
+                    health authorities,  all flights to and from China are suspended - in addition
+                    to those from Wuhan, already suspended by the Chinese authorities.""",  # actually, on February 1 cargo flights were reenabled.
+)
 measures.append(Measure(keyword, begin))
 
+# There are some other measures concercing e.g. thermoscanners in airports, controls for ships, ...
+
+"""
+February 21:
+"""
+begin = datetime.datetime(2020, 2, 21)
+keyword = dict(
+    tag=kw.case_based_isolation,
+    description=""" People returning from risk areas have to signal themselves and are asked to stay in quarantine for 14 days. People having had contact with an infected person have to follow an obligatory 14-day quarantine.""",
+)
+measures.append(Measure(keyword, begin))
+
+# several local / regional measures in Northern Italy on February 23, 25, see
+# https://www.gazzettaufficiale.it/eli/id/2020/02/25/20A01278/sg
+# and
+# https://www.gazzettaufficiale.it/eli/id/2020/02/23/20A01228/sg
+# for Italian law texts
+
+# other regional measures possible
+
+# school and university closures occurred much earlier in popolous regions in the north (Lombardia, Veneto, Emilia-Romagna, maybe also Liguria, Piemonte, Trentino-Alto Adige/Südtirol). This should be taken into account for modelling as the virus spread there (first in Veneto and Lombardy)
 
 """
 March 5:
 """
 begin = datetime.datetime(2020, 3, 5)
+keyword = dict(tag=kw.close_schools, description=""" Nationwide school closures.""")
+measures.append(Measure(keyword, begin))
 keyword = dict(
-    tag=kw.close_schools,
-    description=""" Nationwide school closures."""
-    )
+    tag=kw.close_universities, description=""" Nationwide university closures."""
+)
+measures.append(Measure(keyword, begin))
+
+# 8 march: no more travel from Lombardy and 14 provinces in other regions to other parts of Italy
+# the measures of march 10 came into effect earlier in some regions in the north
+"""
+March 10:
+"""
+begin = datetime.datetime(2020, 3, 10)
+keyword = dict(
+    tag=kw.stay_home_if_cough,
+    description=""" Advice to self-isolate if experiencing symptoms.""",
+)  # this was probably earlier
+measures.append(Measure(keyword, begin))
+
+keyword = dict(
+    tag=kw.forbid_assemblies_all,
+    description=""" The government bans all public events.""",
+)
+measures.append(Measure(keyword, begin))
+
+keyword = dict(
+    tag=kw.keep_distance,
+    description=""" A distance of more than 1m has to be kept and any other form
+                    of alternative aggregation is to be excluded.""",
+)
 measures.append(Measure(keyword, begin))
 
 
@@ -52,48 +96,20 @@ March 10:
 """
 begin = datetime.datetime(2020, 3, 10)
 keyword = dict(
-    tag=kw.stay_home_if_cough,
-    description=""" Advice to self-isolate if experiencing symptoms."""
-    )
-measures.append(Measure(keyword, begin))
-
-keyword = dict(
-    tag=kw.case_based_isolation,
-    description=""" quarantine if tested positive """
-    )
-measures.append(Measure(keyword, begin))
-
-keyword = dict(
-    tag=kw.forbid_assemblies_all,
-    description=""" The government bans all public events."""
-    )
-measures.append(Measure(keyword, begin))
-
-keyword = dict(
-    tag=kw.keep_distance,
-    description=""" A distance of more than 1m has to be kept and any other form
-                    of alternative aggregation is to be excluded."""
-    )
-measures.append(Measure(keyword, begin))
-
-
-"""
-March 11:
-"""
-begin = datetime.datetime(2020, 3, 11)
-keyword = dict(
     tag=kw.lockdown,
     description=""" The government closes all public places. People have to stay
-                    at home except for essential travel."""
-    )
-measures.append(Measure(keyword, begin, end=datetime.datetime(2020, 5, 18)))
+                    at home except for essential travel.""",
+)
+measures.append(
+    Measure(keyword, begin, end=datetime.datetime(2020, 5, 4))
+)  # the lockdown mainly ended on May 4
 
 keyword = dict(
     tag=kw.close_shops,
     description=""" In a video, the President announced  the closure of all commercial
                     and retail activities, with the exception of grocery stores,
-                    basic necessities, pharmacies and parapharmacies."""
-    )
+                    basic necessities, pharmacies and parapharmacies.""",
+)
 measures.append(Measure(keyword, begin, end=datetime.datetime(2020, 5, 18)))
 
 
@@ -103,20 +119,20 @@ March 21:
 begin = datetime.datetime(2020, 3, 21)
 keyword = dict(
     tag="close public places",
-    description=""" The  Minister of Health signed the ordinanceprohibiting: public
+    description=""" The  Minister of Health signed the ordinance prohibiting: public
                     access to parks, villas, play areas and public gardens; to play
                     games or outdoor recreation. It is permitted to carry out individual
-                    motor activities in the vicinity of one's home, provided that,
+                    physical activities in the vicinity of one's home, provided that,
                     in any case, respecting the distance of at least one meter from
-                    each other person."""
-    )
+                    each other person.""",
+)
 measures.append(Measure(keyword, begin, end=datetime.datetime(2020, 5, 4)))
 
 
 """
 March 23:
 """
-begin = datetime.datetime(2020, 3, 21)
+begin = datetime.datetime(2020, 3, 23)
 keyword = dict(
     tag="no travel",
     description=""" A new ordinance has been adopted jointly by the Minister of
@@ -124,6 +140,8 @@ keyword = dict(
                     natural persons from moving or moving with public or private
                     means of transport in a municipality other than that in which
                     they are located, except for proven work needs, of absolute
-                    urgency or for health reasons."""
-    )
-measures.append(Measure(keyword, begin, end=datetime.datetime(2020, 5, 18)))
+                    urgency or for health reasons.""",
+)
+measures.append(
+    Measure(keyword, begin, end=datetime.datetime(2020, 5, 18))
+)  # gradually from May 4
