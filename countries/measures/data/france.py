5a279e90df891931b07f00924a0e5c3a371e0a94
emilIftekhar <iftekharemil@yahoo.de>
2020-06-18 16:21:19 +0200

    Added end of measures for France

--- countries/measures/data/france.py
+++ countries/measures/data/france.py
@@ -1,6 +1,5 @@
 """ # Measures for France
-
-We look at the different measures as can be seen on
+Additional measures can be found on https://www.gouvernement.fr/info-coronavirus/les-actions-du-gouvernement
 """
 
 """ ## Imports
@@ -24,11 +23,23 @@ measures = []  # Create empty array on which we append later on
 
 
 """
+February 29:
+"""
+begin = datetime.datetime(2020, 2, 29)  # according to Wikipedia
+keyword = dict(
+    tag="forbid assemblies > 5000",
+    description=""" ban on gatherings of more than 5000 people in confined spaces.""",
+    references="https://www.gouvernement.fr/info-coronavirus/les-actions-du-gouvernement"
+)
+measures.append(Measure(keyword, begin))
+
+"""
 March 10:
 """
 begin = datetime.datetime(2020, 3, 10)  # according to Wikipedia
 keyword = dict(
-    tag="forbid assemblies > 1000", description=""" Bans of events >1000 people."""
+    tag="forbid assemblies > 1000",
+    description=""" Bans of events >1000 people."""
 )
 measures.append(Measure(keyword, begin))
 
@@ -48,8 +59,9 @@ begin = datetime.datetime(2020, 3, 16)
 keyword = dict(
     tag=kw.close_schools,
     description=""" Nationwide school closures became effective, including universities.""",  # announced on saturday, 14/03, falls essentially together with closure of non-essential services, effective one day earlier
+    references="https://www.gouvernement.fr/info-coronavirus/les-actions-du-gouvernement"
 )
-measures.append(Measure(keyword, begin))
+measures.append(Measure(keyword, begin, end=datetime.datetime(2020, 5, 11))) # Ended gradually
 
 
 """
@@ -60,5 +72,14 @@ keyword = dict(
     tag=kw.lockdown,
     description=""" Everybody has to stay at home. Need a selfauthorisation
                     form to leave home. """,
+    references="https://www.gouvernement.fr/info-coronavirus/les-actions-du-gouvernement"
 )
-measures.append(Measure(keyword, begin))
+measures.append(Measure(keyword, begin, end=datetime.datetime(2020, 5, 11)))
+
+keyword = dict(
+    tag="no travel / close borders",
+    description=""" restriction of travel to the strict minimum in the European Union;
+                    closing the borders of the Schengen area; """,
+    references="https://www.gouvernement.fr/info-coronavirus/les-actions-du-gouvernement"
+)
+measures.append(Measure(keyword, begin, end=datetime.datetime(2020, 5, 11)))
