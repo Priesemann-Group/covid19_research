8c318b3e00f3acbeb8c185a5647a3d1540c1b144
Sebastian <sebastian@mohrenclan.de>
2020-10-27 14:10:18 +0100

    Added a folder to create animated incidence gif of germany.

---
+++ incidence_landkreise/create_figures.py
@@ -0,0 +1,70 @@
+# ------------------------------------------------------------------------------ #
+# @Author:        Sebastian B. Mohr
+# @Email:
+# @Created:       2020-10-27 11:13:12
+# @Last Modified: 2020-10-27 13:31:39
+# ------------------------------------------------------------------------------ #
+import pandas as pd
+import numpy as np
+import geopandas as gpd
+import matplotlib
+import matplotlib.pyplot as plt
+from matplotlib.patches import Patch
+import datetime
+
+groups = ["A00-A04", "A05-A14", "A15-A34", "A35-A59", "A60-A79", "A80+"]
+
+df_places = gpd.read_file("./data/landkreise_simplify200.geojson")
+
+# Color of plot
+cmap = matplotlib.colors.ListedColormap(["#008000", "#ffd700", "#e50000"])
+
+count = 0
+for date in pd.date_range(
+    start=datetime.datetime(2020, 9, 7), end=datetime.datetime.now()
+):
+
+    # Load json
+    try:
+        df = pd.read_json(
+            f"./data/ts/data_{date.strftime('%d_%m')}.json", orient="values"
+        )
+    except Exception as e:
+        break
+
+    df = df.T
+    df.index.name = "RS"
+    df = df.reset_index()
+    df["RS"] = df["RS"].astype(str).apply(lambda x: x.zfill(5))
+
+    merged = df_places.merge(df, left_on="RS", right_on="RS")
+
+    # Create a plot for every agegroup:
+    fig, axes = plt.subplots(2, 3, figsize=(16, 9))
+    for i, age_group in enumerate(groups):
+        if i > 2:
+            k = i - 3
+            j = 1
+        else:
+            k = i
+            j = 0
+        merged.plot(
+            column=f"inzidenz_{age_group}", ax=axes[j, k], cmap=cmap, vmin=0, vmax=75,
+        )
+        axes[j, k].axes.xaxis.set_visible(False)
+        axes[j, k].axes.yaxis.set_visible(False)
+        axes[j, k].set_title(f"{age_group}")
+    # Get legend
+
+    custom_lines = [
+        Patch(facecolor=cmap(0),),
+        Patch(facecolor=cmap(0.5),),
+        Patch(facecolor=cmap(1.1),),
+    ]
+
+    fig.legend(custom_lines, [" 0-25", "25-50", "50+"], title="Fälle/100.000EW")
+    fig.text(0.51, 0.05, date.strftime("%d. %B"), fontsize=20, ha="center")
+    fig.savefig(
+        f"./figures/{str(count).zfill(4)}.png", transparent=True, dpi=300,
+    )
+    count = count + 1
---
+++ incidence_landkreise/time_series.py
@@ -0,0 +1,173 @@
+# ------------------------------------------------------------------------------ #
+# Creates the
+# @Author:        Sebastian B. Mohr
+# @Email:
+# @Created:       2020-10-15 14:08:49
+# @Last Modified: 2020-10-27 12:29:09
+# ------------------------------------------------------------------------------ #
+import ujson
+import pandas as pd
+import covid19_inference as cov19
+import numpy as np
+import datetime
+
+""" # Load data
+"""
+
+# Load data with cov19npis module
+rki = cov19.data_retrieval.RKI()
+rki.download_all_available_data(force_download=True)
+
+
+# Load population data
+population = pd.read_csv(
+    "./data/population_landkreise_altersgruppen.csv", encoding="cp1252",
+)
+# FORMAT BS Should not be neccesary anymore:
+"""
+population = population.dropna()
+population = population.drop(columns="date")
+# Convert ags col to string
+population["ags"] = population["ags"].apply(
+    lambda x: "0" + str(int(x)) if len(str(int(x))) == 4 else str(int(x)),
+)
+
+
+for col in population.columns:
+    population[col] = population[col].map(lambda x: x.strip("+-"))
+    population[col].replace("", np.nan, inplace=True)
+    population = population.dropna()
+    population[col] = population[col].astype(int)
+"""
+
+population = population.set_index(["ags", "region", "typ"])
+
+# Raw geo_file
+with open("./data/population_landkreise.json") as json_file:
+    population_landkreise = ujson.load(json_file)
+
+
+""" # Maniuplate data
+"""
+
+# Get the same age groups as for the rki database:
+pop_rki_aligned = pd.DataFrame()
+
+pop_rki_aligned["A00-A04"] = (
+    population["unter 3 Jahre"] + population["3 bis unter 6 Jahre"]
+)
+pop_rki_aligned["A05-A14"] = (
+    population["6 bis unter 10 Jahre"] + population["10 bis unter 15 Jahre"]
+)
+pop_rki_aligned["A15-A34"] = (
+    population["15 bis unter 18 Jahre"]
+    + population["18 bis unter 20 Jahre"]
+    + population["20 bis unter 25 Jahre"]
+    + population["25 bis unter 30 Jahre"]
+    + population["30 bis unter 35 Jahre"]
+)
+pop_rki_aligned["A35-A59"] = (
+    population["35 bis unter 40 Jahre"]
+    + population["40 bis unter 45 Jahre"]
+    + population["45 bis unter 50 Jahre"]
+    + population["50 bis unter 55 Jahre"]
+    + population["55 bis unter 60 Jahre"]
+)  # Ratio in population
+pop_rki_aligned["A60-A79"] = (
+    population["60 bis unter 65 Jahre"] + population["65 bis unter 75 Jahre"]
+)
+pop_rki_aligned["A80+"] = population["75 Jahre und mehr"]
+pop_rki_aligned["unbekannt"] = pop_rki_aligned.sum(axis=1) * 0.01  # 1% not known
+
+# New cases with rolling 7 day sum:
+data_rki = rki.data
+date = rki.data["date"].max()
+index = pd.date_range(rki.data["date"].min(), rki.data["date"].max())
+
+data_rki = data_rki.set_index(["IdLandkreis", "Altersgruppe", "date",])
+data_rki = data_rki.groupby(["IdLandkreis", "Altersgruppe", "date",]).sum()["confirmed"]
+data_rki = data_rki.groupby(level=[0, 1]).apply(
+    lambda x: x.reset_index(level=[0, 1], drop=True).reindex(index)
+)
+data_rki.index = data_rki.index.set_names("date", level=-1)
+data_rki = data_rki.fillna(0)
+
+cases_7_day_sum = data_rki.rolling(7).sum()
+
+
+""" # Create data dict
+    [IDLandkreis, [cases,incidenc]]
+"""
+dates = pd.date_range(start=datetime.datetime(2020, 9, 7), end=date)
+for date in dates:
+    # We want to store the LAST number of weekly new cases
+    data_cases = cases_7_day_sum.xs(date, level="date")
+
+    data = {}
+    for lk_id in data_cases.index.get_level_values(level="IdLandkreis").unique():
+        data_lk = data_cases.xs(lk_id, level="IdLandkreis")
+
+        # BERLIN
+        berlin = [
+            11002,
+            11003,
+            11004,
+            11005,
+            11006,
+            11007,
+            11008,
+            11009,
+            11010,
+            11011,
+            11012,
+        ]
+        if lk_id in berlin:
+            continue
+
+        if lk_id == 11001:
+            data_lk = data_cases.xs(lk_id, level="IdLandkreis")
+            for id_b in berlin:
+                data_lk += (
+                    data_cases.xs(id_b, level="IdLandkreis")
+                    .reindex(data_lk.index)
+                    .fillna(0)
+                )
+            lk_id = 11000
+
+        # Normal
+        data[lk_id] = {}
+        data[lk_id]["weekly_cases"] = data_lk.sum()
+
+        try:
+            data[lk_id]["inzidenz"] = (
+                data[lk_id]["weekly_cases"]
+                * 100000
+                / population["Insgesamt"].xs(lk_id, level="ags").values[0]
+            )
+        except:
+            data[lk_id]["inzidenz"] = (
+                data[lk_id]["weekly_cases"] * 100000 / population_landkreise[str(lk_id)]
+            )
+
+        # Age groups
+        for ag in data_lk.index:
+            data[lk_id][f"weekly_cases_{ag}"] = data_lk[ag]
+            try:
+                data[lk_id][f"inzidenz_{ag}"] = (
+                    data_lk[ag]
+                    * 100000
+                    / pop_rki_aligned[ag].xs(lk_id, level="ags").values[0]
+                )
+            except:
+                data[lk_id][f"inzidenz_{ag}"] = (
+                    data_lk[ag]
+                    * 100000
+                    / (
+                        pop_rki_aligned.sum()[ag]
+                        / pop_rki_aligned.sum().sum()
+                        * population_landkreise[str(lk_id)]
+                    )
+                )
+    # Write file
+    with open(f"./data/ts/data_{date.strftime('%d_%m')}.json", "w") as outfile:
+        ujson.dump(data, outfile)
