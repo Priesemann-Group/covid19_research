4388967c3e6100ba9f2d22ca7981d520507dbe70
Sebastian <sebastian@mohrenclan.de>
2020-11-16 14:38:54 +0100

    Uploaded script for the second heatmap and updated heatmap.py file.

--- circuit_breaker/Plotting/heatmap.py
+++ circuit_breaker/Plotting/heatmap.py
@@ -2,7 +2,7 @@
 # @Author:        F. Paul Spitzner
 # @Email:         paul.spitzner@ds.mpg.de
 # @Created:       2020-11-14 17:15:43
-# @Last Modified: 2020-11-16 13:23:17
+# @Last Modified: 2020-11-16 14:16:55
 # ------------------------------------------------------------------------------ #
 import os
 import sys
@@ -27,7 +27,7 @@ import numpy as np
 import seaborn as sns  # v 0.11
 import pandas as pd
 
-import covid19_inference as cov19
+
 import datetime
 from datetime import datetime as dt
 
@@ -83,58 +83,55 @@ def reverse_cmap(cmap, name="my_cmap_r"):
     return my_cmap_r
 
 
-cmap = reverse_cmap(cmap_to_mpl(custom_cmaps["cold"]))
+if __name__ == "__main__":
+    cmap = reverse_cmap(cmap_to_mpl(custom_cmaps["cold"]))
 
-t = np.loadtxt("/Users/paul/Downloads/Ts_LD.csv", delimiter=",")
-k = np.loadtxt("/Users/paul/Downloads/kt_LD.csv", delimiter=",")
-d = np.loadtxt("/Users/paul/Downloads/D_LD.csv", delimiter=",")
+    t = np.loadtxt("/Users/paul/Downloads/Ts_LD.csv", delimiter=",")
+    k = np.loadtxt("/Users/paul/Downloads/kt_LD.csv", delimiter=",")
+    d = np.loadtxt("/Users/paul/Downloads/D_LD.csv", delimiter=",")
 
+    fig, ax = plt.subplots(figsize=(6, 4))
 
+    # sns.heatmap(
+    #     data=d,
+    #     ax=ax,
+    #     cmap=cmap,
+    #     vmin=0,
+    #     vmax=9,
+    #     xticklabels=k,
+    #     yticklabels=t,
+    #     square=False,
+    #     cbar_ax=ax_bar,
+    # )
+    # ax.invert_yaxis()
 
-fig, ax = plt.subplots(figsize=(6, 4))
+    fig, ax = plt.subplots()
+    levels = np.arange(-0.25, 9.75, 0.5)
+    CS = ax.contourf(k, t, d, levels, cmap=cmap, origin="lower")
+    CS2 = ax.contour(CS, levels=[4, 8], colors=["white", "black"], origin="lower")
 
-# sns.heatmap(
-#     data=d,
-#     ax=ax,
-#     cmap=cmap,
-#     vmin=0,
-#     vmax=9,
-#     xticklabels=k,
-#     yticklabels=t,
-#     square=False,
-#     cbar_ax=ax_bar,
-# )
-# ax.invert_yaxis()
+    # Make a colorbar for the ContourSet returned by the contourf call.
+    cbar = fig.colorbar(CS, ticks=np.arange(0, 10))
+    cbar.ax.set_ylabel("required lockdown (weeks)")
+    # Add the contour line levels to the colorbar
+    cbar.add_lines(CS2)
 
+    ax.set_ylabel("start time Ts")
+    ax.set_xlabel("contact fraction k")
 
-fig, ax = plt.subplots()
-levels = np.arange(-0.25, 9.75, 0.5)
-CS = ax.contourf(k, t, d, levels, cmap=cmap, origin='lower')
-CS2 = ax.contour(CS, levels=[4,8], colors=['white', 'black'], origin='lower')
+    fig.tight_layout()
+    fig.savefig("/Users/paul/Desktop/heatmap.pdf")
 
-# Make a colorbar for the ContourSet returned by the contourf call.
-cbar = fig.colorbar(CS, ticks=np.arange(0,10))
-cbar.ax.set_ylabel('required lockdown (weeks)')
-# Add the contour line levels to the colorbar
-cbar.add_lines(CS2)
-
-ax.set_ylabel("start time Ts")
-ax.set_xlabel("contact fraction k")
-
-fig.tight_layout()
-fig.savefig("/Users/paul/Desktop/heatmap.pdf")
-
-
-# redo everyting for png and remove clutter
-fig, ax = plt.subplots()
-levels = np.arange(-0.25, 9.75, 0.5)
-CS = ax.contourf(k, t, d, levels, cmap=cmap, origin='lower')
-CS2 = ax.contour(CS, levels=[4,8], colors=['white', 'black'], origin='lower')
-fig.tight_layout()
-plt.gca().set_axis_off()
-ax.axis("off")
-plt.subplots_adjust(top=1, bottom=0, right=1, left=0, hspace=0, wspace=0)
-plt.margins(0, 0)
-plt.gca().xaxis.set_major_locator(plt.NullLocator())
-plt.gca().yaxis.set_major_locator(plt.NullLocator())
-fig.savefig("/Users/paul/Desktop/heatmap.png", dpi=600)
+    # redo everyting for png and remove clutter
+    fig, ax = plt.subplots()
+    levels = np.arange(-0.25, 9.75, 0.5)
+    CS = ax.contourf(k, t, d, levels, cmap=cmap, origin="lower")
+    CS2 = ax.contour(CS, levels=[4, 8], colors=["white", "black"], origin="lower")
+    fig.tight_layout()
+    plt.gca().set_axis_off()
+    ax.axis("off")
+    plt.subplots_adjust(top=1, bottom=0, right=1, left=0, hspace=0, wspace=0)
+    plt.margins(0, 0)
+    plt.gca().xaxis.set_major_locator(plt.NullLocator())
+    plt.gca().yaxis.set_major_locator(plt.NullLocator())
+    fig.savefig("/Users/paul/Desktop/heatmap.png", dpi=600)
---
+++ circuit_breaker/Plotting/n_equilibrium.py
@@ -0,0 +1,171 @@
+# ------------------------------------------------------------------------------ #
+# Shows equilibrium with influx event
+# Needs files created by the cpp model -> see Cpp_Implementation folder
+#
+# Runtime ~ 1min
+# ------------------------------------------------------------------------------ #
+
+
+import pandas as pd
+import os
+import matplotlib.pyplot as plt
+from matplotlib.ticker import FuncFormatter
+import numpy as np
+import sys
+import seaborn as sns
+from matplotlib.colors import LogNorm
+from heatmap import reverse_cmap, cmap_to_mpl
+import matplotlib as mpl
+
+# default fonts
+mpl.rcParams["font.sans-serif"] = "Arial"
+mpl.rcParams["font.family"] = "sans-serif"
+
+custom_cmaps = dict()
+custom_cmaps["viridis"] = [
+    (0, "white"),
+    (0.1, "#a9db33"),
+    (0.45, "#23898d"),
+    (0.85, "#3f4688"),
+    (1, "black"),
+]
+
+
+# Load data
+def get_var(filename, var_name, foldername="short_swipe"):
+    # Load file
+    df = pd.read_csv(f"../Cpp_Implementation/data/{foldername}/{filename}")
+    df["time"] = df["time"]
+    df = df.set_index("time")
+    return df[var_name]
+
+
+def ts_plot(axes, df, x_min, x_max, color="tab:blue", label=""):
+    axes.plot(df.index, df, color=color, label=label)
+    axes.set_xlim(x_min, x_max)
+
+
+def load_data_1(foldername):
+    # ------------------------------------------------------------------------------ #
+    # Loading data
+    # ------------------------------------------------------------------------------ #
+    Is, ks, Phis, dfs = [], [], [], []
+    N = []
+    for filename in os.listdir(f"../Cpp_Implementation/data/{foldername}/"):
+        if filename.endswith(".csv"):
+            k, Phi = os.path.splitext(filename)[0].split("_")
+
+            new_cases = get_var(filename, "New cases observed", foldername=foldername)
+            # print(new_cases.iloc[950])
+            # if np.abs(new_cases.iloc[950] - new_cases.iloc[951]) < 1e-4:
+            ks.append(k)
+            Phis.append(Phi)
+            N.append(new_cases.iloc[2800])
+            dfs.append(new_cases)
+
+    data = pd.DataFrame()
+    data["Phi"] = Phis
+    data["k"] = ks
+    data["N_equilibrium"] = N
+    # data["new_cases"] = dfs
+    data = data.astype("float64")
+
+    data = data.set_index(["Phi", "k"])
+    data = data.sort_index()
+    return data
+
+
+def load_data_2(foldername):
+    # ------------------------------------------------------------------------------ #
+    # Loading data
+    # ------------------------------------------------------------------------------ #
+    Is, ks, Phis = [], [], []
+    N = []
+    for filename in os.listdir(f"../Cpp_Implementation/data/{foldername}/"):
+        if filename.endswith(".csv"):
+            I, k, Phi = os.path.splitext(filename)[0].split("_")
+            new_cases = get_var(filename, "New cases observed", foldername=foldername)
+
+            Is.append(I)
+            ks.append(k)
+            Phis.append(Phi)
+            N.append(new_cases.iloc[2800])
+    data = pd.DataFrame()
+    data["Phi"] = Phis
+    data["k"] = ks
+    data["N_equilibrium"] = N
+    # data["new_cases"] = dfs
+    data = data.astype("float64")
+
+    return data
+
+
+if __name__ == "__main__":
+
+    # data_slice = load_data_1("short_swipe")
+    # data_heatmap = load_data_2("variate_k_Phi")
+
+    data_heatmap = pd.read_csv("heatmap_eq.csv")
+    data_slice = pd.read_csv("slices_eq.csv")
+    data_heatmap["k"] = 1.0 - data_heatmap["k"]
+    data_slice["k"] = 1.0 - data_slice["k"]
+    data_slice = data_slice.set_index(["Phi", "k"])
+    cmap = reverse_cmap(cmap_to_mpl(custom_cmaps["viridis"]))
+
+    # ------------------------------------------------------------------------------ #
+    # Figure
+    # ------------------------------------------------------------------------------ #
+
+    fig, axes = plt.subplots(1, 2, figsize=(6.5, 2.8), constrained_layout=True)
+
+    # First Phi = 0.1
+    phi01 = data_slice.xs(0.100, level="Phi")
+    phi1 = data_slice.xs(1.000, level="Phi")
+    phi10 = data_slice.xs(10.000, level="Phi")
+
+    # Slice no log scale
+    axes[0].plot(
+        phi01.index, phi01["N_equilibrium"], label=r"$\Phi = 0.1$", color="tab:green"
+    )
+    axes[0].plot(
+        phi1.index, phi1["N_equilibrium"], label=r"$\Phi = 1$", color="tab:orange"
+    )
+    axes[0].plot(
+        phi10.index, phi10["N_equilibrium"], label=r"$\Phi = 10$", color="tab:red"
+    )
+    axes[0].set_ylim(0, 200)
+    axes[0].set_xlabel(r"Contact reduction $k$")
+    axes[0].set_ylabel(r"$N_{eq}$")
+    axes[0].legend()
+
+    # Heatmap
+    Z = data_heatmap.pivot_table(
+        index="k", columns="Phi", values="N_equilibrium"
+    ).T.values
+    X_unique = np.sort(data_heatmap["k"].unique())
+    Y_unique = np.sort(data_heatmap["Phi"].unique())
+    X, Y = np.meshgrid(X_unique, Y_unique)
+
+    levels = np.arange(-5, 100, 5)
+    CS = axes[1].contourf(
+        X,
+        Y,
+        Z,
+        levels,
+        cmap=cmap,
+        # locator=mpl.ticker.LogLocator(),
+        origin="lower",
+    )
+    # CS2 = axes[1].contour(CS, levels=[4, 8], colors=["white", "black"], origin="lower")
+    cbar = fig.colorbar(CS, ticks=np.arange(0, 100, 10))
+    cbar.ax.set_ylabel("$N_{eq}$")
+    # cbar.add_lines(CS2)
+
+    axes[0].invert_xaxis()
+    axes[0].set_ylim(0, 150)
+    axes[1].invert_xaxis()
+    axes[1].set_ylabel(r"Influx $\Phi$")
+    axes[1].set_xlabel(r"Contact fraction $k$")
+
+    fig.savefig("figures/fig_equilibrium.svg", transparent=True, dpi=300)
+    fig.savefig("figures/fig_equilibrium.pdf", transparent=True, dpi=300)
